ec0b99b439b61936e508c154eb9b7bb5417d0e2c
Howard Butler <howard@hobu.co>
2016-11-03 09:41:58 -0500

    use new-style cmake find_package for PDAL

--- config-hobu.sh
+++ config-hobu.sh
@@ -2,8 +2,8 @@ GENERATOR="Unix Makefiles"
 
 BUILD_TYPE=Release
 
-cmake -G "$GENERATOR" \
-  -DPDAL_INCLUDE_DIR=../pdal/include \
-  -DPDAL_LIBRARY=../pdal/lib/libpdal.dylib \
+cmake . -G "$GENERATOR" \
+  -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
   -DCMAKE_INSTALL_PREFIX=./package \
+  -DPDAL_DIR=/Users/hobu/pdal-build/lib/pdal/cmake  \
       .
